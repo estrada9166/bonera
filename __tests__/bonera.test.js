e01845787d21a5e141af749dfa1f9298c6012752
Alejandro Estrada <alejandroestrada@MBPdeAlejandro.lan>
2017-07-04 16:56:02 -0500

    test

--- __tests__/bonera.test.js
+++ __tests__/bonera.test.js
@@ -1,8 +1,6 @@
 const bonera = require('../lib/bonera')
 const app = bonera();
-const http = require('http');
-const querystring = require('querystring');
-const request = require('request');
+const {getTest, postTest, putTest, patchTest, deleteTest} = require('../test_methods/methods');
 
 
 app.get('/', (req, res) => {
@@ -72,104 +70,10 @@ app.delete('/restaurant/:id', (req, res) => {
 })
 
 app.use((req, res) => {
-  console.log('hello')
+  console.log('Hello')
 })
 
 app.listen(3000);
-
-const getTest = (name, path, expected) => {
-  test(name, () => {
-    return new Promise (resolve => {
-      http.get(`http://localhost:3000${path}`, res => {
-        let data = '';
-        res.on('data', _data => data += _data);
-        res.on('end', () => resolve(data));
-      })
-    })
-    .then((data) => {
-      expect(data).toEqual(expected)
-    })
-  })
-};
-
-const postTest = (name, path, postWord, expected) => {
-  test(name, () => {
-    return new Promise (resolve => {
-      request({
-        url: `http://localhost:3000${path}`,
-        method: 'POST',
-        headers: {
-          'User-Agent':   'Super Agent/0.0.1',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        form: {'key': postWord}
-      }, (err, res, body) => {
-        resolve(body)
-      })
-    })
-    .then(data => {
-      expect(data).toEqual(expected)    
-    })
-  })
-}
-
-const putTest = (name, path, putWord, expected) => {
-  test(name, () => {
-    return new Promise(resolve => {
-      request({
-        url: `http://localhost:3000${path}`,
-        method: 'PUT',
-        headers: {
-          'User-Agent':   'Super Agent/0.0.1',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        form: {'key': putWord}
-      }, (err, res, body) => {
-        resolve(body)
-      })
-    })
-    .then(data => {
-      expect(data).toEqual(expected)
-    })
-  })
-}
-
-const patchTest = (name, path, putWord, expected) => {
-  test(name, () => {
-    return new Promise(resolve => {
-      request({
-        url: `http://localhost:3000${path}`,
-        method: 'PATCH',
-        headers: {
-          'User-Agent':   'Super Agent/0.0.1',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        form: {'key': putWord}
-      }, (err, res, body) => {
-        resolve(body)
-      })
-    })
-    .then(data => {
-      expect(data).toEqual(expected)
-    })
-  })
-}
-
-const deleteTest = (name, path, expected) => {
-  test(name, () => {
-    return new Promise(resolve => {
-      request({
-        url: `http://localhost:3000${path}`,
-        method: 'DELETE'
-      }, (err, res, body) => {
-        resolve(body)
-      })
-    })
-    .then(data => {
-      expect(data).toEqual(expected)
-    })
-  })
-}
 
 getTest('get route to root path', '/', 'root path');
 getTest('get route to users path', '/users', 'users path');
